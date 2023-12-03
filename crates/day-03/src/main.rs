47c0c84d15a218a13c5482b481b9f8c46d517ae1
Jon Minter <jdiminter@gmail.com>
2023-12-03 16:23:51 -0500

    Day 3 part 2

--- crates/day-03/src/main.rs
+++ crates/day-03/src/main.rs
@@ -19,8 +19,16 @@ impl NumberNode {
         symbol.x >= min_x && symbol.x <= max_x && symbol.y >= min_y && symbol.y <= max_y
     }
 }
+
+#[derive(PartialEq, Debug, Eq)]
+enum SymbolType {
+    Gear,
+    Other,
+}
+
 #[derive(PartialEq, Debug)]
 struct SymbolNode {
+    sym_type: SymbolType,
     x: usize,
     y: usize,
 }
@@ -30,6 +38,11 @@ struct Line {
     symbols: Vec<SymbolNode>,
     numbers: Vec<NumberNode>,
 }
+
+#[derive(PartialEq, Eq, Debug, Hash)]
+struct PartNumber(u32);
+#[derive(PartialEq, Eq, Debug, Hash)]
+struct GearRatio(u32);
 
 /// Find all part numbers in a line.
 /// We only need the symbols that are on the current, prev & next lines (if they exist)
@@ -41,8 +54,8 @@ fn find_part_numbers(
     prev_line: Option<&Line>,
     current_line: &Line,
     next_line: Option<&Line>,
-) -> Vec<u32> {
-    let all_symbols: Vec<&SymbolNode> = prev_line
+) -> Vec<PartNumber> {
+    let search_symbols: Vec<&SymbolNode> = prev_line
         .map(|l| l.symbols.iter())
         .unwrap_or_default()
         .chain(current_line.symbols.iter())
@@ -53,28 +66,70 @@ fn find_part_numbers(
         .numbers
         .iter()
         .filter(|number| {
-            all_symbols
+            search_symbols
                 .iter()
                 .filter(|symbol| number.is_adjacent_to(symbol))
                 .count()
                 > 0
         })
-        .map(|number| number.value)
+        .map(|number| PartNumber(number.value))
         .collect()
 }
 
-fn find_all_part_numbers(mut lines: impl Iterator<Item = Line>) -> Vec<u32> {
+fn find_gear_ratios(
+    prev_line: Option<&Line>,
+    current_line: &Line,
+    next_line: Option<&Line>,
+) -> Vec<GearRatio> {
+    let search_numbers: Vec<&NumberNode> = prev_line
+        .map(|l| l.numbers.iter())
+        .unwrap_or_default()
+        .chain(current_line.numbers.iter())
+        .chain(next_line.map(|l| l.numbers.iter()).unwrap_or_default())
+        .collect();
+
+    let gear_symbols = current_line
+        .symbols
+        .iter()
+        .filter(|s| s.sym_type == SymbolType::Gear);
+
+    let mut gear_ratios = vec![];
+    for gear_sym in gear_symbols {
+        let adjacent_numbers: Vec<&&NumberNode> = search_numbers
+            .iter()
+            .filter(|number| number.is_adjacent_to(gear_sym))
+            .collect();
+
+        if adjacent_numbers.len() == 2 {
+            gear_ratios.push(GearRatio(
+                adjacent_numbers[0].value * adjacent_numbers[1].value,
+            ));
+        }
+    }
+
+    gear_ratios
+}
+
+fn find_all_part_numbers_and_gear_ratios(
+    mut lines: impl Iterator<Item = Line>,
+) -> (Vec<PartNumber>, Vec<GearRatio>) {
     let maybe_first_line = lines.next();
     match maybe_first_line {
         Some(first_line) => {
             let mut prev_line = None;
             let mut current_line = first_line;
 
-            let mut part_numbers = Vec::new();
+            let mut part_numbers = vec![];
+            let mut gear_ratios = vec![];
 
             loop {
                 let next_line = lines.next();
                 part_numbers.extend(find_part_numbers(
+                    prev_line.as_ref(),
+                    &current_line,
+                    next_line.as_ref(),
+                ));
+                gear_ratios.extend(find_gear_ratios(
                     prev_line.as_ref(),
                     &current_line,
                     next_line.as_ref(),
@@ -87,12 +142,13 @@ fn find_all_part_numbers(mut lines: impl Iterator<Item = Line>) -> Vec<u32> {
                 }
             }
 
-            part_numbers
+            (part_numbers, gear_ratios)
         }
-        None => vec![],
+        None => (vec![], vec![]),
     }
 }
 
+const GEAR_SYMBOL: char = '*';
 const PERIOD: char = '.';
 const BASE_10: u32 = 10;
 
@@ -153,7 +209,12 @@ fn parse_line_into_symbols_and_numbers(line: &str, y: usize) -> Line {
             }
 
             if c != PERIOD {
-                symbols.push(SymbolNode { x, y });
+                let sym_type = if c == GEAR_SYMBOL {
+                    SymbolType::Gear
+                } else {
+                    SymbolType::Other
+                };
+                symbols.push(SymbolNode { sym_type, x, y });
             }
             None
         }
@@ -171,11 +232,13 @@ fn main() {
         .enumerate()
         .map(|(y, line)| parse_line_into_symbols_and_numbers(&line.unwrap(), y));
 
-    let part_numbers = find_all_part_numbers(lines);
+    let (part_numbers, gear_ratios) = find_all_part_numbers_and_gear_ratios(lines);
 
-    let part_numbers_sum: u32 = part_numbers.iter().sum();
+    let part_numbers_sum: u32 = part_numbers.iter().map(|p| p.0).sum();
+    let gear_ratios_sum: u32 = gear_ratios.iter().map(|g| g.0).sum();
 
-    println!("{}", part_numbers_sum);
+    println!("Sum of part numbers: {}", part_numbers_sum);
+    println!("Sum of gear ratios: {}", gear_ratios_sum);
 }
 
 #[cfg(test)]
@@ -183,14 +246,19 @@ mod test {
     use std::collections::HashSet;
 
     use crate::{
-        find_all_part_numbers, parse_line_into_symbols_and_numbers, Line, NumberNode, SymbolNode,
+        find_all_part_numbers_and_gear_ratios, parse_line_into_symbols_and_numbers, GearRatio,
+        Line, NumberNode, PartNumber, SymbolNode, SymbolType,
     };
 
-    fn get_expected_part_numbers() -> Vec<u32> {
+    fn get_expected_part_numbers() -> Vec<PartNumber> {
         [467, 35, 633, 617, 592, 755, 664, 598]
             .iter()
-            .cloned()
+            .map(|n| PartNumber(*n))
             .collect()
+    }
+
+    fn get_expected_gear_ratios() -> Vec<GearRatio> {
+        vec![GearRatio(16345), GearRatio(451490)]
     }
 
     fn get_test_cases() -> Vec<(&'static str, Line)> {
@@ -218,7 +286,11 @@ mod test {
             (
                 "...*......",
                 Line {
-                    symbols: vec![SymbolNode { x: 3, y: 0 }],
+                    symbols: vec![SymbolNode {
+                        sym_type: crate::SymbolType::Gear,
+                        x: 3,
+                        y: 0,
+                    }],
                     numbers: vec![],
                 },
             ),
@@ -245,14 +317,22 @@ mod test {
             (
                 "......#...",
                 Line {
-                    symbols: vec![SymbolNode { x: 6, y: 0 }],
+                    symbols: vec![SymbolNode {
+                        sym_type: SymbolType::Other,
+                        x: 6,
+                        y: 0,
+                    }],
                     numbers: vec![],
                 },
             ),
             (
                 "617*......",
                 Line {
-                    symbols: vec![SymbolNode { x: 3, y: 0 }],
+                    symbols: vec![SymbolNode {
+                        sym_type: SymbolType::Gear,
+                        x: 3,
+                        y: 0,
+                    }],
                     numbers: vec![NumberNode {
                         value: 617,
                         start_x: 0,
@@ -264,7 +344,11 @@ mod test {
             (
                 ".....+.58.",
                 Line {
-                    symbols: vec![SymbolNode { x: 5, y: 0 }],
+                    symbols: vec![SymbolNode {
+                        sym_type: SymbolType::Other,
+                        x: 5,
+                        y: 0,
+                    }],
                     numbers: vec![NumberNode {
                         value: 58,
                         start_x: 7,
@@ -300,7 +384,18 @@ mod test {
             (
                 "...$.*....",
                 Line {
-                    symbols: vec![SymbolNode { x: 3, y: 0 }, SymbolNode { x: 5, y: 0 }],
+                    symbols: vec![
+                        SymbolNode {
+                            sym_type: SymbolType::Other,
+                            x: 3,
+                            y: 0,
+                        },
+                        SymbolNode {
+                            sym_type: SymbolType::Gear,
+                            x: 5,
+                            y: 0,
+                        },
+                    ],
                     numbers: vec![],
                 },
             ),
@@ -344,7 +439,11 @@ mod test {
             (
                 "123*456",
                 Line {
-                    symbols: vec![SymbolNode { x: 3, y: 0 }],
+                    symbols: vec![SymbolNode {
+                        sym_type: SymbolType::Gear,
+                        x: 3,
+                        y: 0,
+                    }],
                     numbers: vec![
                         NumberNode {
                             value: 123,
@@ -382,8 +481,9 @@ mod test {
 
     #[test]
     fn test_finds_right_part_numbers() {
-        let part_numbers =
-            find_all_part_numbers(get_test_cases().into_iter().map(|(_, line)| line));
+        let (part_numbers, _) = find_all_part_numbers_and_gear_ratios(
+            get_test_cases().into_iter().map(|(_, line)| line),
+        );
 
         let expected_part_numbers = get_expected_part_numbers();
 
@@ -396,11 +496,32 @@ mod test {
 
     #[test]
     fn test_sums_part_numbers() {
-        let part_numbers =
-            find_all_part_numbers(get_test_cases().into_iter().map(|(_, line)| line));
+        let (part_numbers, _) = find_all_part_numbers_and_gear_ratios(
+            get_test_cases().into_iter().map(|(_, line)| line),
+        );
 
-        let part_numbers_sum: u32 = part_numbers.iter().sum();
+        let part_numbers_sum: u32 = part_numbers.iter().map(|p| p.0).sum();
 
         assert_eq!(part_numbers_sum, 4361);
+    }
+
+    #[test]
+    fn test_parses_gear_ratios() {
+        let (_, gear_ratios) = find_all_part_numbers_and_gear_ratios(
+            get_test_cases().into_iter().map(|(_, line)| line),
+        );
+
+        assert_eq!(gear_ratios, get_expected_gear_ratios());
+    }
+
+    #[test]
+    fn test_sums_gear_ratios() {
+        let (_, gear_ratios) = find_all_part_numbers_and_gear_ratios(
+            get_test_cases().into_iter().map(|(_, line)| line),
+        );
+
+        let gear_ratios_sum: u32 = gear_ratios.iter().map(|g| g.0).sum();
+
+        assert_eq!(gear_ratios_sum, 467835);
     }
 }
